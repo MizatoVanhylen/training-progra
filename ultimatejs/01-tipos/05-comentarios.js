cc067e3b920029508660944e8862be3177088cd4
MizatoVanhylen <denisse.romero.c@gmail.com>
2024-09-26 16:28:13 -0300

    se agregan ejercicios de comentarios, objetos, arrays y funciones

---
+++ ultimatejs/01-tipos/05-comentarios.js
@@ -0,0 +1,14 @@
+// comentarios
+/*
+todo lo que se escriba entre medio de los
+slash y los asteriscos
+va a ser ignorado 
+*/
+/**
+ * Este es un comentario
+ * de 
+ * muchas lineas
+ */
+let numero = 42; // este es otro comentario
+
+console.log(numero);
