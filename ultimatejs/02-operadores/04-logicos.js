ffdcc5f41c94e1689990627d925f997f336bdb1d
MizatoVanhylen <denisse.romero.c@gmail.com>
2024-09-27 13:37:31 -0300

    se agregan ejercicios de operadores de ccomparacion, logicos, falsy, birwise, orden de operaciones y ioerador ternario

---
+++ ultimatejs/02-operadores/04-logicos.js
@@ -0,0 +1,17 @@
+ // AND, OR, NOT
+
+// AND &&
+
+let mayor = false;
+let suscrito = true;
+//console.log(true && true);
+//console.log(false && true);
+console.log("operados and", mayor && suscrito);
+
+// OR || 
+console.log("operados or", mayor || suscrito);
+
+// NOT !
+
+console.log("operados not", !mayor);
+let soloCatalogoInfantil = !mayor;
