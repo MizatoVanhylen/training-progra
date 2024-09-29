de7dbea0f5f0c264baa6eaa81d9c815c7e6e353a
MizatoVanhylen <denisse.romero.c@gmail.com>
2024-09-29 16:34:21 -0300

    se agregar ultimos ejercicios de control de flujo, y se comienza con ejercicios generales

---
+++ ultimatejs/03-control-flujo/10-switch.js
@@ -0,0 +1,12 @@
+let accion = "actializar";
+
+switch (accion) {
+    case "listar":
+      console.log("accion de listar");
+      break;
+    case "guardar":
+      console.logC("accion de guardar");
+      break;
+    default:
+      console.log("accion no reconocida");
+}
