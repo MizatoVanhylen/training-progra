c64a89c51a815f04c99d5137369b2584a10c94d8
MizatoVanhylen <denisse.romero.c@gmail.com>
2024-10-29 16:14:13 -0300

    avancez batalla pokemon, elección ataque jugador y ataque cpu

--- ejerciciosderefuerzo/ejercicio-while-03.js
+++ ejerciciosderefuerzo/ejercicio-while-03.js
@@ -27,32 +27,32 @@ let pokemones = [
  {
     nombre: "Eevee",
     vida: 80,
-    ataques: {
-      atk1: {nombre: "Antojo", daño: 25},
-      atk2: {nombre: "Placaje", daño: 40},
-      atk3: {nombre: "Ataque Rapido",daño: 40},
-      atk4: {nombre: "Mordisco",daño: 40}
-    }
+    ataques: [
+     {nombre: "Antojo", daño: 25},
+     {nombre: "Placaje", daño: 40},
+     {nombre: "Ataque Rapido",daño: 40},
+     {nombre: "Mordisco",daño: 40}
+    ]
   },
   {
     nombre: "Espeon",
     vida: 160,
-    ataques: {
-      atk1: {nombre: "Confusión", daño: 50},
-      atk2: {nombre: "Placaje", daño: 40},
-      atk3: {nombre: "Psicorrayo", daño: 65},
-      atk4: {nombre: "Premonición", daño: 120}
-    }
-  },
+    ataques: [
+     {nombre: "Confusión", daño: 50},
+     {nombre: "Placaje", daño: 40},
+     {nombre: "Psicorrayo", daño: 65},
+     {nombre: "Premonición", daño: 120}
+    ],
+  },  
  {
     nombre: "Umbreon",
     vida: 160,
-    ataques: {
-      atk1: {nombre: "Persecución", daño: 40},
-      atk2: {nombre: "Ataque Rapido", daño: 40},
-      atk3: {nombre: "Finta", daño: 60},
-      atk4: {nombre: "Última Baza", daño: 140}
-    }
+    ataques: [
+      {nombre: "Persecución", daño: 40},
+      {nombre: "Ataque Rapido", daño: 40},
+      {nombre: "Finta", daño: 60},
+      {nombre: "Última Baza", daño: 140}
+    ]
   }
 ]
 
@@ -60,34 +60,37 @@ let pokemonesEnemigos = [
   {
     nombre: "Pichu",
     vida: 80,
-    ataques: {
-      atk1: {nombre: "Doble Bofetón", daño: 15},
-      atk2: {nombre: "Megapuño", daño: 80},
-      atk3: {nombre: "Golpe Cabeza", daño: 70},
-      atk4: {nombre: "Megapatada", daño: 120}
-    }
+    ataques: [
+       {nombre: "Doble Bofetón", daño: 15},
+       {nombre: "Megapuño", daño: 80},
+       {nombre: "Golpe Cabeza", daño: 70},
+       {nombre: "Megapatada", daño: 120}
+    ]
   },
  {
     nombre: "Pikachu",
     vida: 160,
-    ataques: {
-      atk1: {nombre: "Doble Bofeton", daño: 15},
-      atk2: {nombre: "Megapuño", daño: 80},
-      atk3: {nombre: "Megapatada", daño: 120},
-      atk4: {nombre: "Doble Filo",  daño: 120}
-    }
+    ataques: [
+       {nombre: "Doble Bofeton", daño: 15},
+       {nombre: "Megapuño", daño: 80},
+       {nombre: "Megapatada", daño: 120},
+       {nombre: "Doble Filo",  daño: 120}
+    ]
   },
   {
     nombre: "Raichu",
     vida: 180,
-    ataques: {
-      atk1: {nombre: "Doble Bofetón", daño: 15},
-      atk2: {nombre: "Atizar", daño: 80},
-      atk3: {nombre: "Golpe Cabeza", daño: 70},
-      atk4: {nombre: "Doble Filo", daño: 120}
-    }
+    ataques: [
+       {nombre: "Doble Bofetón", daño: 15},
+       {nombre: "Atizar", daño: 80},
+       {nombre: "Golpe Cabeza", daño: 70},
+       {nombre: "Doble Filo", daño: 120}
+    ]
   }
 ];
+
+/* Elección de Pokémon jugador*/
+
 while (elegirpokemon === true) {
   const pokemon = prompt("Elige tu Pokémon:\n 1 : Eevee \n 2 : Espeon \n 3 : Umbreon \n 4 : No deseo elegir un Pokémon");
  if (pokemon == 1) {
@@ -111,21 +114,26 @@ while (elegirpokemon === true) {
   console.log(jugador)
 }
 
+/* Elección del Pokémon Random*/
 const enemigo = Math.floor(Math.random() * 3);
 jugadorEnemigo = pokemonesEnemigos[enemigo];
 alert("El enemigo a elegido ha " + jugadorEnemigo.nombre);
 
-const ataques = prompt("Elige entre estos ataques:\n 1 : " + jugador.ataques.atk1.nombre + "\n 2 : " + jugador.ataques.atk2.nombre + "\n 3 : " + jugador.ataques.atk3.nombre + "\n 4 : " + jugador.ataques.atk4.nombre  );
-alert("Haz elegido " + );
+/* Elección de ataques jugador*/
+
+let ataques = prompt("Elige entre estos ataques:\n 1 : " + jugador.ataques[0].nombre + "\n 2 : " + jugador.ataques[1].nombre + "\n 3 : " + jugador.ataques[2].nombre + "\n 4 : " + jugador.ataques[3].nombre);
+ataques = ataques -1;
+alert("Usas " + jugador.ataques[ataques].nombre + " que hace " + jugador.ataques[ataques].daño + " de daño para atacar a " + jugadorEnemigo.nombre);
+
+/* Elección ataque random*/
+
+const ataqueEnemigo = Math.floor(Math.random() * 4);
+jugadorEnemigo = pokemonesEnemigos[ataqueEnemigo]
+alert("El enemigo usa " + jugador.ataques[ataqueEnemigo].nombre + " que hace " + jugador.ataques[ataqueEnemigo].daño + " de daño para atacar a " + jugador.nombre);
 
 
 
 
 
-/*const arr = ["eevee", "espeon"]
-const ob = {
-  nombre: "Juan",
-  signo: "cancer"
-}
 
-const arr2 = [{nombre: "juan", signo: "cancer"},{nombre: "Alex", signo: "tauro"},]*/
+
